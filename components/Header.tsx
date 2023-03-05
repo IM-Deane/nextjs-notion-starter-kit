34fab9c284bfa87c7510cc1230e3aa7570dd74e9
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-03-05 13:13:33 -0700

    Removed nested 'a' in Link

--- components/Header.tsx
+++ components/Header.tsx
@@ -139,19 +139,22 @@ function AvatarContainer({ className = '', ...props }) {
 
 function Avatar({ large = false, className = '', ...props }) {
   return (
-    <Link href='/' passHref aria-label='Home' {...props}>
-      <a className={clsx(className, 'pointer-events-auto')}>
-        <Image
-          src={avatarImage}
-          alt='A headshot of Tristan Deane'
-          sizes={large ? '4rem' : '2.25rem'}
-          className={clsx(
-            'rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
-            large ? 'h-16 w-16' : 'h-9 w-9'
-          )}
-          priority
-        />
-      </a>
+    <Link
+      href='/'
+      aria-label='Home'
+      className={clsx(className, 'pointer-events-auto')}
+      {...props}
+    >
+      <Image
+        src={avatarImage}
+        alt='A headshot of Tristan Deane'
+        sizes={large ? '4rem' : '2.25rem'}
+        className={clsx(
+          'rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
+          large ? 'h-16 w-16' : 'h-9 w-9'
+        )}
+        priority
+      />
     </Link>
   )
 }
