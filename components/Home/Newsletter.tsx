701405733fc5a3354a5eaab1d734ff280b87b9dd
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2023-03-06 15:53:34 -0700

    Fixed lint errors

--- components/Home/Newsletter.tsx
+++ components/Home/Newsletter.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Button } from '@/components/Button'
 
-import { MailIcon } from './utils/icons'
+import { MailIcon } from '../utils/icons'
 
 export function Newsletter() {
   return (
